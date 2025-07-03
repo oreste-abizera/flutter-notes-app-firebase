5ba597d6a017d7b1aa5fca7211d489b6d913957e
oreste-abizera <oresteabizera11@gmail.com>
2025-07-04 01:29:59 +0200

    Implement user and note models

---
+++ lib/models/app_user.dart
@@ -0,0 +1,16 @@
+class AppUser {
+  final String uid;
+  final String email;
+
+  AppUser({
+    required this.uid,
+    required this.email,
+  });
+
+  factory AppUser.fromFirebaseUser(user) {
+    return AppUser(
+      uid: user.uid,
+      email: user.email ?? '',
+    );
+  }
+}
