afd6ba0fb6ab400896161eca402db13b98ed849b
oreste-abizera <oresteabizera11@gmail.com>
2025-07-05 21:44:50 +0200

    refactor: streamline AppUser constructor and add Validators utility class

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -2,15 +2,9 @@ class AppUser {
   final String uid;
   final String email;
 
-  AppUser({
-    required this.uid,
-    required this.email,
-  });
+  AppUser({required this.uid, required this.email});
 
   factory AppUser.fromFirebaseUser(user) {
-    return AppUser(
-      uid: user.uid,
-      email: user.email ?? '',
-    );
+    return AppUser(uid: user.uid, email: user.email ?? '');
   }
 }
